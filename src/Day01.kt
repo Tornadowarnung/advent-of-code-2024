f30bdf6e4f4d68da5b520405f056ae9d06e2740f
Tornadowarnung <tornadowarnung@protonmail.com>
2024-12-02 15:37:00 +0100

    solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,27 @@
+import kotlin.math.absoluteValue
+
 fun main() {
+    fun splitIntoLeftAndRightLists(input: List<String>) = input.map { line ->
+        val nums = line.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
+        nums.first() to nums.last()
+    }.unzip()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = splitIntoLeftAndRightLists(input)
+
+        return left.sorted().zip(right.sorted()).sumOf { (it.first - it.second).absoluteValue }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (left, right) = splitIntoLeftAndRightLists(input)
+
+        return left.sumOf { it.absoluteValue * right.count { r -> r == it } }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
