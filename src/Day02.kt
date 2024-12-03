f4a8213d03ef6c9c83452814ed7aa9dbf85066b8
Tornadowarnung <tornadowarnung@protonmail.com>
2024-12-03 10:58:43 +0100

    add ugly solution for day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,29 +1,40 @@
 import kotlin.math.absoluteValue
 
 fun main() {
+    fun checkSingleReport(report: List<Int>): Boolean {
+        val differences = mutableListOf<Int>()
+        report.forEachIndexed { index, value ->
+            if (index == 0) {
+                return@forEachIndexed
+            }
+            differences.add(report[index - 1] - value)
+        }
+        val isSameIncrease = differences.all { it < 0 } || differences.all { it > 0 }
+        val isNotTooHighOrTooLow = differences.map { it.absoluteValue }.none { it > 3 || it < 1 }
+        return isSameIncrease && isNotTooHighOrTooLow
+    }
+
     fun part1(input: List<String>): Int {
         return input.count { reportString ->
             val report = reportString.split(" ").map { it.toInt() }
-            val differences = mutableListOf<Int>()
-            report.forEachIndexed { index, value ->
-                if (index == 0) {
-                    return@forEachIndexed
-                }
-                differences.add(report[index - 1] - value)
-            }
-            val isSameIncrease = differences.all { it < 0 } || differences.all { it > 0 }
-            val isNotTooHighOrTooLow = differences.map { it.absoluteValue }.none { it > 3 || it < 1 }
-            isSameIncrease && isNotTooHighOrTooLow
+            checkSingleReport(report)
         }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return input.count { reportString ->
+            val report = reportString.split(" ").map { it.toInt() }
+            val variants = mutableListOf(report)
+            for(i in report.indices) {
+                variants.add(report.subList(0, i) + report.subList(i + 1, report.size))
+            }
+            variants.any { checkSingleReport(it) }
+        }
     }
 
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 2)
-//    check(part2(testInput) == 31)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day02")
     part1(input).println()
