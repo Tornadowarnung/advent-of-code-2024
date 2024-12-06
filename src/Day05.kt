db8208e9d4a6a162113a8f8f697a8ac530c1f358
Tornadowarnung <tornadowarnung@protonmail.com>
2024-12-06 14:50:48 +0100

    solve day 5 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -26,12 +26,47 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return -1
+        val rulesString = input.subList(0, input.indexOf(""))
+
+        val rules = rulesString.flatMap { rules ->
+            return@flatMap rules.split(",").map { rule ->
+                val ruleParts = rule.split("|")
+                ruleParts[0].toInt() to ruleParts[1].toInt()
+            }
+        }
+        val predecessorByPage = rules.groupBy({ it.second }, { it.first })
+
+        val printsString = input.subList(input.indexOf("") + 1, input.size)
+        val prints = printsString.map { prints -> prints.split(",").map { it.toInt() } }
+        val wrongPrints = prints.filter { print ->
+            print.any { page ->
+                predecessorByPage[page]?.any { predecessor ->
+                    print.indexOf(predecessor) >= print.indexOf(page)
+                } == true
+            }
+        }
+
+        val correctedPrints = mutableListOf<List<Int>>()
+        for (wrongPrint in wrongPrints) {
+            val correctedPrint = mutableListOf<Int>()
+            val pagesToBeOrdered = wrongPrint.toMutableSet()
+            while (pagesToBeOrdered.isNotEmpty()) {
+                val nextCorrectPage = pagesToBeOrdered.find { page ->
+                    !predecessorByPage.containsKey(page) || predecessorByPage[page]!!.all { predecessor ->
+                        !wrongPrint.contains(predecessor) || correctedPrint.contains(predecessor)
+                    }
+                }
+                correctedPrint.add(nextCorrectPage!!)
+                pagesToBeOrdered.remove(nextCorrectPage)
+            }
+            correctedPrints.add(correctedPrint)
+        }
+        return correctedPrints.sumOf { print -> print[print.size / 2] }
     }
 
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 143)
-//    check(part2(testInput) == 9)
+    check(part2(testInput) == 123)
 
     val input = readInput("Day05")
     part1(input).println()
