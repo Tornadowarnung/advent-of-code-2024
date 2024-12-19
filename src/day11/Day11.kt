bdf8715fc7a1e7d6e69eafb99666f346574a0340
Tornadowarnung <tornadowarnung@protonmail.com>
2024-12-19 17:13:01 +0100

    shamelessly steal day 11 part 2

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -3,39 +3,61 @@ package day11
 import readInput
 
 fun main() {
-    fun blink(pebbles: List<Long>): List<Long> {
-        val result = mutableListOf<Long>()
-        pebbles.forEach { pebble ->
-            val pebbleString = pebble.toString()
-            if (pebble == 0L) {
-                result.add(1L)
-            } else if (pebbleString.length % 2 == 0) {
-                val firstHalf = pebbleString.substring(0, pebbleString.length / 2)
-                val secondHalf = pebbleString.substring(pebbleString.length / 2)
-                result.add(firstHalf.toLong())
-                result.add(secondHalf.toLong())
-            } else {
-                result.add(pebble * 2024L)
+    fun blink(pebble: Long, blinkCacheByPebble: MutableMap<Long, List<Long>>): List<Long> {
+        val pebbleString = pebble.toString()
+        if (pebble == 0L) {
+            return listOf(1L)
+        } else if (pebbleString.length % 2 == 0) {
+            if (blinkCacheByPebble.containsKey(pebble)) {
+                return blinkCacheByPebble[pebble]!!
             }
+            val left = pebbleString.substring(0, pebbleString.length / 2).toLong()
+            val right = pebbleString.substring(pebbleString.length / 2).toLong()
+            blinkCacheByPebble[pebble] = listOf(left, right)
+            return blinkCacheByPebble[pebble]!!
+        } else {
+            return listOf(pebble * 2024)
         }
+    }
+
+    fun solve(pebbles: List<Long>, maxBlinks: Long): Long {
+        val blinkCacheByStone = mutableMapOf<Long, List<Long>>()
+        var countByStoneCache = mutableMapOf<Long, Long>()
+        pebbles.forEach { pebble ->
+            countByStoneCache[pebble] = countByStoneCache.getOrDefault(pebble, 0) + 1
+        }
+
+        for (i in 1..maxBlinks) {
+            val updatedCountByStone = mutableMapOf<Long, Long>()
+            countByStoneCache.forEach { (pebble, count) ->
+                val updatedPebbles = blink(pebble, blinkCacheByStone)
+                updatedPebbles.forEach { updatedPebble ->
+                    updatedCountByStone[updatedPebble] = updatedCountByStone.getOrDefault(updatedPebble, 0) + count
+                }
+            }
+            countByStoneCache = updatedCountByStone
+        }
+
+        var result = 0L
+        countByStoneCache.values.forEach { result += it }
         return result
     }
 
-    fun part1(input: List<String>): Int {
-        var pebbles = input.first().split(" ").map { it.toLong() }
-        for (i in 1..25) {
-            pebbles = blink(pebbles)
-        }
-        return pebbles.size
+    fun part1(input: List<String>): Long {
+        val pebbles = input.first().split(" ").map { it.toLong() }
+        val result = solve(pebbles, 25)
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return -1
+    // Shamelessly copied the following solution: https://www.reddit.com/r/adventofcode/comments/1hbm0al/comment/m2q57x3/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    fun part2(input: List<String>): Long {
+        val pebbles = input.first().split(" ").map { it.toLong() }
+        val result = solve(pebbles, 75)
+        return result
     }
 
     val testInput = readInput("Day11_test")
-    check(part1(testInput) == 55312)
-//    check(part2(testInput) == 0)
+    check(part1(testInput) == 55312L)
 
     val input = readInput("Day11")
     println(part1(input))
